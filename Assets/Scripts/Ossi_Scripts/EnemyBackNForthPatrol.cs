4da5a267a40df7f7bac83638fe7002d10a23854b
Manninen Ossi TTK17SP <ossimanninen1@kamk.fi>
2017-11-09 16:39:52 +0200

    enemy patrol updates

--- Assets/Scripts/Ossi_Scripts/EnemyBackNForthPatrol.cs
+++ Assets/Scripts/Ossi_Scripts/EnemyBackNForthPatrol.cs
@@ -2,28 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyPatrol : MonoBehaviour
-{
+public class EnemyBackNForthPatrol : MonoBehaviour {
     public Transform[] Waypoints;
     public float Speed;
     public int currentWP;
+    public bool goPatrol = true;
+    private bool goBack = false;
+    public Vector3 Target;
+    public Vector3 MoveDirection;
+    public Vector3 Velocity;
     // Use this for initialization
-    void Start()
+    void Start ()
     {
         currentWP = 0;
         transform.position = Waypoints[currentWP].position;
-    }
-
-    // Update is called once per frame
-    void Update()
+	}
+	// Update is called once per frame
+	void Update ()
     {
         if (transform.position == Waypoints[currentWP].position)
         {
+            if (goBack)
+                currentWP--;
+            else
                 currentWP++;
         }
         if (currentWP >= Waypoints.Length)
         {
             currentWP = 0;
+        }
+        else if(currentWP == 0)
+        {
+            goBack = false;
         }
         transform.position = Vector3.MoveTowards(transform.position, Waypoints[currentWP].position, Speed * Time.deltaTime);
     }
