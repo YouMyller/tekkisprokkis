237f107328168e9d159366178209f20f78396acb
Ossi Manninen <ossi.sakari.manninen@gmail.com>
2017-11-17 19:48:24 +0200

    script tempfix

--- Assets/Scripts/Ossi_Scripts/RaycastTest.cs
+++ Assets/Scripts/Ossi_Scripts/RaycastTest.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RaycastTest : MonoBehaviour
 {
-
+    /*
     // Use this for initialization
     void Start()
     {
@@ -28,5 +28,5 @@ public class RaycastTest : MonoBehaviour
         {
             print("There is something in front of the object!");
         }
-    }
+    }*/
 }
