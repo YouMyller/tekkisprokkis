e2cfe9ed5f96c69dea48c7989495beabb2fcb1d1
Ossi Manninen <ossi.sakari.manninen@gmail.com>
2017-11-19 22:26:01 +0200

    raycast detection done, small fixes might need to be made

--- Assets/Scripts/Ossi_Scripts/RaycastTest.cs
+++ Assets/Scripts/Ossi_Scripts/RaycastTest.cs
@@ -4,29 +4,40 @@ using UnityEngine;
 
 public class RaycastTest : MonoBehaviour
 {
-    /*
+    public GameObject player;
+    public float aggroRange;
+    private Ray Sight;
+    public float damping;
+
+    private bool detected;
+
     // Use this for initialization
     void Start()
     {
 
     }
 
-    // Update is called once per frame
-    public Transform target;
-
-    // prints "close" if the z-axis of this transform looks
-    // almost towards the target
-
     void FixedUpdate()
     {
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        Sight.origin = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Sight.direction = transform.forward;
         RaycastHit hit;
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        Vector3 fwd = transform.TransformDirection(Vector3.right);
 
-        if(Physics.Raycast(spotter.transform.position, spotter.transform.forward, out hit, 1000.0f))
-
-        if (hit.collider.tag == "player")
+        if (distance < aggroRange)
         {
-            print("There is something in front of the object!");
+            Quaternion rotation = Quaternion.LookRotation(player.transform.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+            if (Physics.Raycast(Sight, out hit, aggroRange))
+            {
+                Debug.DrawRay(Sight.origin, transform.forward * aggroRange, Color.red);
+                if (hit.collider.tag == "Player")
+                {
+                    print("Pew!");
+                }
+            }
         }
-    }*/
+    }
+
 }
