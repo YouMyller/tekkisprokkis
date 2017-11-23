60c75fa33a26e259f197806ac92e152c460700fe
Manninen Ossi TTK17SP <ossimanninen1@kamk.fi>
2017-11-23 14:01:13 +0200

    first proto fo main menu

---
+++ Assets/Scripts/Ossi_Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+    public bool isStart;
+    public bool isCredits;
+    public bool isExit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            if (isStart)
+            {
+                print("Start the game! Yay!");
+            }
+            if (isCredits)
+            {
+                print("We made this game!");
+            }
+            if (isExit)
+            {
+                print("Please don't go :(");
+            }
+        }
+    }
+}
